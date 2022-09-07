0d498cdc6e2611129a547783f004ac049658c680
marlonalvss <marlon14br@gmail.com>
2022-09-07 15:33:48 -0300

    :construction: alteração Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Footer(){
-    
-    return <footer className="cont2">
-    <div className="center">
-  </div>
+function Footer() {
+
+  return <footer className="cont2">
+      <form className="fFooter">
+        <p className="pFooter">Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
+        <div className="fOrganiza">  
+        <input className="fName" type="text" placeholder="Nome Completo" name="nome" />
+        <input className="fEmail" type="email" placeholder="Insira seu e-mail" name="email_usuario" />
+       </div> 
+      <button className="fSubmit">teste do teste</button>
+      <p className="p2Footer">Testando suas habilidades em HTML, CSS e JS. Resilia 2022</p>
+      </form>
   </footer>
 }
 
